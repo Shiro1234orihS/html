4b184efe63543c8be550cd97a336a5683ef082ac
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-05-03 21:11:21 +0100

    j'ai avencé sur le site sur le resporant

--- LARAVEL/chediego/resources/views/welcome.blade.php
+++ LARAVEL/chediego/resources/views/welcome.blade.php
@@ -6,11 +6,55 @@
 
 @section('content')   
 <section>
-    <div class="container">
-        <input required type="text" name="username" class="input" id="username"> <!-- Ajout de l'id pour le label -->
-        <label for="username" class="label">Username</label> <!-- Associé à l'input via 'for' -->
-    </div>  
-    <h1>sakl</h1>
+    <div class="slide-container">
+            <div class="custom-slider fade">
+                <div class="slide-index">1 / 2</div>
+                <img class="slide-img" src="{{ asset('/picture/resto/image1.avif') }}">
+                <div class="slide-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+            </div>
+            <div class="custom-slider fade">
+                <div class="slide-index">2 / 2</div>
+                <img class="slide-img" src="{{ asset('/picture/resto/image2.jpg') }}">
+                <div class="slide-text">Nullam luctus aliquam ornare. </div>
+            </div>
+            <a class="prev" onclick="plusSlides(-1)">❮</a>
+            <a class="next" onclick="plusSlides(1)">❯</a>
+        </div>
+        <br>
+        <div class="slide-dot">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+        </div>
     
 </section>
 @endsection
+
+
+<script>
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("custom-slider");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    }
+</script>
