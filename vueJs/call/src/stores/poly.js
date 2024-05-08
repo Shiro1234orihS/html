904f69362bb1f4eafbea7233a50d93ebee0c0106
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-05-08 14:09:58 +0100

    j'ai fais l'envoie la recherche de API

--- vueJs/call/src/stores/poly.js
+++ vueJs/call/src/stores/poly.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 export const polyStore = defineStore('poly', () => {
   const apiKey = '4JoXvRoJGqkmzriveyO3hBVWkLn3sgWr';
-  const ticker = 'NVDA';  // Remplacer par le symbole boursier de l'entreprise
-  const url = `https://api.polygon.io/v1/meta/symbols/${ticker}/company?apiKey=${apiKey}`;
+  const ticker = 'NVIDIA';  // Remplacer par le symbole boursier de l'entreprise
+  const url = `https://api.polygon.io/v3/reference/tickers?search=${ticker}&active=true&sort=ticker&order=asc&limit=10&apiKey=${apiKey}`;
 
   const marketData = ref(null);  // Utilisez cette référence pour stocker les données du marché récupérées
 
@@ -18,5 +18,15 @@ export const polyStore = defineStore('poly', () => {
     });
   }
 
-  return { marketData, fetchMarketData }
+  function research(name){
+    
+    axios.get(`https://api.polygon.io/v3/reference/tickers?search=${name}&active=true&sort=ticker&order=asc&limit=10&apiKey=${apiKey}`).then(response => {
+      marketData.value = response.data;  // Stockez les données du marché dans la référence Vue
+      console.log("Données du marché récupérées avec succès:", marketData.value);
+    }).catch(error => {
+      console.error("Erreur lors de la récupération des données du marché:", error);
+    });
+  }
+
+  return { marketData, fetchMarketData, research }
 });
--- vueJs/call/src/views/HomeView.vue
+++ vueJs/call/src/views/HomeView.vue
@@ -5,10 +5,12 @@ import { ref, computed, watch  } from 'vue'
 
 const poly = polyStore()
 
+const  cherche = ref("")
 
 
-
-
+function recherche(){
+  poly.research(cherche.value);
+}
 
 function test() {
   poly.fetchMarketData();
@@ -19,9 +21,62 @@ function test() {
 
 <template>
   <main>
-    <button @click="test">test</button>
+    <div id = "bar">
+      <input placeholder="Enter your text..." class="input" name="text" type="text"  v-model="cherche">
+      <button @click="recherche"  class="button">Cherche</button>
+    </div>
+    
     <TheWelcome />
 
     
+    
   </main>
 </template>
+
+<style scoped>
+#bar{
+  position: absolute;
+  left : 40%
+}
+
+.input {
+  background-color: #212121;
+  max-width: 500px;
+  height: 20px;
+  width:300px;
+  padding: 10px;
+  /* text-align: center; */
+  border: 2px solid white;
+  border-radius: 5px;
+  /* box-shadow: 3px 3px 2px rgb(249, 255, 85); */
+}
+
+.input:focus {
+  color: rgb(0, 255, 255);
+  background-color: #212121;
+  outline-color: rgb(0, 255, 255);
+  box-shadow: -3px -3px 15px rgb(0, 255, 255);
+  transition: .1s;
+  transition-property: box-shadow;
+}
+
+.button{
+  background-color: #212121;
+  color: white;
+  margin-left: 10px;
+  height:40px;
+  padding: 10px;
+
+  border: 2px solid white;
+  border-radius: 5px;
+}
+
+.button:hover {
+  color: rgb(0, 255, 255);
+  background-color: #212121;
+  outline-color: rgb(0, 255, 255);
+  box-shadow: -3px -3px 15px rgb(0, 255, 255);
+  transition: .1s;
+  transition-property: box-shadow;
+}
+</style>
