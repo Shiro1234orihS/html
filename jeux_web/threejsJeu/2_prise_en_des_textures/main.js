c3321a2ac9bae3a6c309cef6420cebbbe7fb0240
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-06-05 18:21:59 +0100

    mise à jour

---
+++ jeux_web/threejsJeu/2_prise_en_des_textures/main.js
@@ -0,0 +1,70 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+let object;
+let controls;
+let objToRender = 'models';
+let mouseX = 0, mouseY = 0; // Déclaration des variables mouseX et mouseY
+
+const loader = new GLTFLoader();
+loader.load(
+    './assets/models/scene.gltf',
+    function (gltf) {
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+camera.position.z = 25;
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, 1);
+scene.add(ambientLight);
+
+controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // Activer le lissage
+controls.dampingFactor = 0.25; // Facteur de lissage
+controls.enableZoom = true; // Activer le zoom
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (object && objToRender === "eye") {
+        object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+        object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+    }
+
+    controls.update(); // Mettre à jour les contrôles
+    renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+document.onmousemove = (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
+animate();
---
+++ jeux_web/threejsJeu/README.md
@@ -0,0 +1,3 @@
+# threejsJeu
+
+dz
