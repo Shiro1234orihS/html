a8fda73d0ce150a0dce30d576ec7f794cd70c617
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-07-25 09:57:27 +0100

    j'ai la forme du calendirer

---
+++ angular/earningsCall/src/app/componants/earnings-call/earnings-call.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-earnings-call',
+  standalone: true,
+  imports: [],
+  templateUrl: './earnings-call.component.html',
+  styleUrl: './earnings-call.component.scss'
+})
+
+export class EarningsCallComponent {
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+}
--- angular/earningsCall/src/app/page/home/home.component.ts
+++ angular/earningsCall/src/app/page/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EarningsCallComponent} from './../../componants/earnings-call/earnings-call.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EarningsCallComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
