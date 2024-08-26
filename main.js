4ec2324bc5769cdeb6ddb69b3336b606b3938e33
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-08-26 20:48:35 +0100

    j'ai changé la souris sur le souris et il y a moins de faute d'orthographe

--- main.js
+++ main.js
@@ -8,34 +8,56 @@ if (window.innerWidth > 600) {
         };
     });
 }
-//test foi"zenfzoifnzfoizn
 
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.section');
     const navLinks = document.querySelectorAll('.sidebar a');
-    
+    let cursor = document.querySelector(".custom-cursor");
+    let cursorbefore = document.querySelector(".custom-cursor-before");
+
     navLinks.forEach(link => {
       link.addEventListener('click', (e) => {
         e.preventDefault();
         
-  
         sections.forEach(section => section.classList.add('hidden'));
-        
         
         const id = link.getAttribute('href').substring(1); 
         document.getElementById(id).classList.remove('hidden');
       });
+
+      // Ajouter un événement pour changer l'apparence du curseur lorsqu'il survole un lien
+      link.addEventListener('mouseover', () => {
+        cursor.style.transform = 'scale(1.5)';
+        cursorbefore.style.transform = 'scale(1.5)';
+        cursor.style.backgroundColor = 'white'; 
+        cursorbefore.style.backgroundColor = 'rgba(255, 255, 255, 0.2)'; 
+      });
+
+      link.addEventListener('mouseout', () => {
+        cursor.style.transform = 'scale(1)';
+        cursorbefore.style.transform = 'scale(1)';
+        cursor.style.backgroundColor = 'black'; // Revenir à la couleur originale
+        cursorbefore.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'; // Revenir à la couleur originale
+
+      });
+    });
+    
+    document.addEventListener("mousemove",function(e){
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+  
+        cursorbefore.style.left = e.clientX + 'px';
+        cursorbefore.style.top = e.clientY + 'px';
     });
 });
 
 function ouvrirPDF() {
- 
-  window.open('ressource/cv/cv_anglais.pdf');
+    window.open('ressource/cv/cv_anglais.pdf');
 }
+
 function ouvrirPDF1() {
- 
     window.open('ressource/cv/cv_français.pdf');
-  }
+}
 
 $(document).ready(function(){
     $('.qr-btn').click(function(e) {
@@ -72,19 +94,13 @@ $(document).ready(function(){
 });
 
 document.getElementById('francais').addEventListener('click', function() {
-
-
     window.location.href = 'index.html'; // Redirige vers la version français
 });
 
 document.getElementById('portugais').addEventListener('click', function() {
-
-
     window.location.href = 'index-pt.html'; // Redirige vers la version portugais
 });
 
 document.getElementById('anglais').addEventListener('click', function() {
-
     window.location.href = 'index-an.html'; // Redirige vers la version anglaise
 });
-
