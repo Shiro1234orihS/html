3e4d9a9435137a3d7650698d3963d9999424b6b3
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-09-23 22:50:37 +0100

    j'ai ajouter les dates automatique dans les inputs

--- angular/earningsCall/front/src/app/componants/filtre/filtre.component.ts
+++ angular/earningsCall/front/src/app/componants/filtre/filtre.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { addMonths, format } from 'date-fns';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filtre',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule], 
   templateUrl: './filtre.component.html',
   styleUrl: './filtre.component.scss'
 })
 export class FiltreComponent {
+
+  public dateduJour: FormControl;
+  public datedeFin: FormControl;
+  public nbentreprise: FormControl;
+
+  constructor(){
+    const aujourdhui = new Date();
+    const startDate: string = aujourdhui.toISOString().split('T')[0];
+
+    const dateDansUnMois: Date = addMonths(aujourdhui, 1);
+    const endDate: string = format(dateDansUnMois, 'yyyy-MM-dd');
+
+
+    this.dateduJour = new FormControl(startDate);
+    this.datedeFin = new FormControl(endDate);
+    this.nbentreprise = new FormControl(60);
+  }
 
 }
